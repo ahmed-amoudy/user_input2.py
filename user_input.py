9f4ef58964b109ec1765c856d90f8f3b09c6b99b
ahmed-amoudy <ahmedtwalib35@gmail.com>
2024-07-15 15:16:29 +0300

    Plp assignment

---
+++ user_input.py
@@ -0,0 +1,4 @@
+Name = input(' What is your Name?')
+Age = input(' How old are You?')
+Location = input(' Your current location?')
+print('Hello',Name,'you are',Age,'years old and live in',Location)
